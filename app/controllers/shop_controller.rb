6b07f0434290c4d3d5586b1fe8432ca76fd165cd
josemourinho333 <yoo.phil92@gmail.com>
2022-08-31 16:36:43 -0700

    Add sort by asc and desc feature

--- app/controllers/shop_controller.rb
+++ app/controllers/shop_controller.rb
@@ -1,15 +1,31 @@
 class ShopController < ApplicationController
 
   def all
-    response = HTTParty.get('https://fakestoreapi.com/products')
-    pp response
-    @products = JSON.parse(response.body)
+    sort = params[:sort ? :sort : nil]
+    if !sort
+      response = HTTParty.get('https://fakestoreapi.com/products')
+      @products = JSON.parse(response.body)
+    else
+      response = HTTParty.get("https://fakestoreapi.com/products", query: {
+        sort: "#{sort}"
+      })
+      @products = JSON.parse(response.body)
+    end
   end
 
   def filter
-    filter = params[:filterBy]
-    response = HTTParty.get("https://fakestoreapi.com/products/category/#{filter}")
-    @filtered = JSON.parse(response.body)
+    sort = params[:sort ? :sort : nil]
+    if !sort
+      filter = params[:filterBy]
+      response = HTTParty.get("https://fakestoreapi.com/products/category/#{filter}")
+      @filtered = JSON.parse(response.body)
+    else 
+      filter = params[:filterBy]
+      response = HTTParty.get("https://fakestoreapi.com/products/category/#{filter}", query: {
+        sort: "#{sort}"
+      })
+      @filtered = JSON.parse(response.body)
+    end
   end
 
 end
